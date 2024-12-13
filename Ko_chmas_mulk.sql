9e381a88ec54a1f8a57d469917182703361c12a0
Orzubek93 <orzumurodnorbutayev71@gmail.com>
2024-12-13 15:35:01 +0500

    Kochmas mulk

---
+++ Ko_chmas_mulk.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 13, 2024 at 10:33 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Ko'chmas_mulk_oldi-sottisi_bilan_shug'illanuvchi_tashkilot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Ko'chmas_mulk`
+--
+
+CREATE TABLE `Ko'chmas_mulk` (
+  `Id` int(11) NOT NULL,
+  `Mulk` int(11) NOT NULL,
+  `Narxi` int(11) NOT NULL,
+  `Soni` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
